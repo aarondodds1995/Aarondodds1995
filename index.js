f5222dff51e3b74ac9bbeabbcc1e3fa0a268b2b4
aarondodds1995 <azzadodds@gmail.com>
2026-05-03 17:06:03 +0100

    index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector('#entryGrid');
   if (!grid) return;
+
+const entries = JSON.parse(localStorage.getItem('entries')) || [];
+  grid.innerHTML = '';
+
+  const recentEntries = entries.slice(-5).reverse();

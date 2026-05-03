e95f5c2eab20c0150454dbbb170ad97d836f5729
aarondodds1995 <azzadodds@gmail.com>
2026-05-03 20:36:45 +0100

    index.js

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ const entries = JSON.parse(localStorage.getItem('entries')) || [];
   grid.innerHTML = '';
 
   const recentEntries = entries.slice(-5).reverse();
+
+                      
+  recentEntries.forEach(entry => {
+    const card = document.createElement('div');
+    card.classList.add('card');
+
+    card.innerHTML = `
+      <h3>${entry.title}</h3>
+      <p>${entry.content}</p>
+      <small>${entry.date}</small>
+    `;
+
+    grid.appendChild(card);
+  });
+});

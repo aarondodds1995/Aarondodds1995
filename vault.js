1f5f5111d6dd0539851d75f835f399d0cf644048
aarondodds1995 <azzadodds@gmail.com>
2026-05-05 21:01:42 +0100

    vault.js

--- vault.js
+++ vault.js
@@ -1,1 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+ const container = document.querySelector('#vaultContainer');
+
+  console.log("Vault loaded");
+console.log("BEFORE:", localStorage.getItem("entries"));
+
+  let entries = JSON.parse(localStorage.getItem('entries')) || [];

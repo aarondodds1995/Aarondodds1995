ce604a82db5e9ab7f09c75d24512936c19c8286b
aarondodds1995 <azzadodds@gmail.com>
2026-05-05 21:59:20 +0100

    vault.js

--- vault.js
+++ vault.js
@@ -6,3 +6,11 @@ document.addEventListener("DOMContentLoaded", () => {
 console.log("BEFORE:", localStorage.getItem("entries"));
 
   let entries = JSON.parse(localStorage.getItem('entries')) || [];
+
+ console.log("Vault entries:", entries);
+
+  container.innerHTML = '';
+
+  entries.reverse().forEach(entry => {
+    const row = document.createElement('div');
+    row.classList.add('entry-row');
